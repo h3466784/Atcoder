8150a68c526b47bcd6d6c78bde5dcce4b8a34489
h3466784 <h3466784@gmail.com>
2023-12-30 10:13:41 +0900

    add b.c

---
+++ smtb2019/b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int N;
+  scanf("%d", &N);
+
+  for (int i=1; i<=N; i++) {
+    if ((int)(i*1.08) == N) {
+      printf("%d\n", i);
+      return 0;
+    }
+  }
+
+  printf(":(\n");
+}
